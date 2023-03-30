089d918e1b587f8a12e48e61228381b999348ca6
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-30 10:58:02 +0300

    solution to task 3, project 0x00-python_variable_annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def to_str(n: float) -> str:
+    """Converts a float to a string
+
+    Args:
+        n (float): number to convert
+
+    Returns:
+        str: converted number
+    """
+    return str(n)
