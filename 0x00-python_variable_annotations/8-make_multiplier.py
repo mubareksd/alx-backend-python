4b6400b707eaa5097de0f02646ef14253f55a99f
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-30 12:35:52 +0300

    fixed type annotation

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function that multiplies a float by multiplier
 
     Args:
