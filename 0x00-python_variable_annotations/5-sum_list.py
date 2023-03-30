c6a951ec713a2402ec0f8d0493d8bb30b6a6798d
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-30 11:11:35 +0300

    solution to task 5, project 0x00-python_variable_annotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum of a list of floats
+
+    Args:
+        input_list (List[float]): list of floats
+
+    Returns:
+        float: sum of the list
+    """
+    return float(sum(input_list))
