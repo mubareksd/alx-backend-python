d97310e111f023384d088aa52b7d4a631f273642
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-30 12:34:32 +0300

    fixed type annotation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """9-element_length module
 """
-from typing import List, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: List[Tuple]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """returns a list of tuples with the first value as a string and the second
 
     Args:
