4fec3de908f641fa0c8143b1631f0dc5b8a62644
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-30 10:15:08 +0300

    added comment for the method add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,13 @@
 
 
 def add(a: float, b: float) -> float:
-    """returns the sum of two floats
+    """add two floats
+
+    Args:
+        a (float): float number 1
+        b (float): float number 2
+
+    Returns:
+        float: sum of a and b
     """
     return a + b
