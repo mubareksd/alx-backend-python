9f1456ed047408c264c1c6bc11280359e0aa749f
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-30 11:30:33 +0300

    solution to task 9, project 0x00-python_variable_annotation

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from typing import List, Tuple
+
+
+def element_length(lst: List[Tuple]) -> List[Tuple[str, int]]:
+    """returns a list of tuples with the first value as a string and the second
+
+    Args:
+        lst (List[Tuple]): list of tuples
+
+    Returns:
+        List[Tuple[str, int]]: list of tuples with the first value as a string
+    """
+    return [(i, len(i)) for i in lst]
