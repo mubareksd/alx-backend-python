c1d74204ae3757f45f2b9935f508ffbe42a416bd
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-30 10:14:32 +0300

    solution to task 1, project 0x00-python_variable_annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenates two strings
+
+    Args:
+        str1 (str): string 1
+        str2 (str): string 2
+
+    Returns:
+        str: concatenated string
+    """
+    return str1 + str2
