55aded718d04645feb65a6e7ac9dfb03b63308e5
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-30 11:15:57 +0300

    solution to task 6, project 0x00-python_variable_annotation

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """sum of a list of floats and integers
+
+    Args:
+        mxd_list (List[Union[int, float]]): list of floats and integers
+
+    Returns:
+        float: sum of the list
+    """
+    return float(sum(mxd_list))
