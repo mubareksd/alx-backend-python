25ee8a1e9b10406dbaba9b5651ce20e8222067ce
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-24 12:04:08 +0300

    solution to task 3, project 0x03-unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -69,11 +69,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(
-            TestClass,
-            'a_method',
-            return_value=42
-        ) as mock_method:
+        with patch.object(TestClass, 'a_method') as mock_method:
+            mock_method.return_value = 42
             test_class = TestClass()
             self.assertEqual(test_class.a_property, 42)
             self.assertEqual(test_class.a_property, 42)
