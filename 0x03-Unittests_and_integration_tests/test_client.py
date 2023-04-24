f1f04ef84673223ba06b5eccbb8694bb389937a4
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-24 22:44:54 +0300

    solution to task 6, project 0x03-unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -36,7 +36,20 @@ class TestGithubOrgClient(unittest.TestCase):
                    new_callable=PropertyMock) as mock_org:
             mock_org.return_value = {'repos_url': 'google'}
             client = GithubOrgClient('google')
-            self.assertEqual(client._public_repos_url, mock_org.return_value['repos_url'])
+            self.assertEqual(client._public_repos_url,
+                             mock_org.return_value['repos_url'])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """test_public_repos function
+        """
+        mock_get_json.return_value = [{'name': 'google'}, {'name': 'abc'}]
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = 'google'
+            client = GithubOrgClient('google')
+            self.assertEqual(client.public_repos(), ['google', 'abc'])
+            mock_get_json.assert_called_once()
 
 
 if __name__ == "__main__":
