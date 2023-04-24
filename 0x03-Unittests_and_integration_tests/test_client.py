caaeef94e50237121df3544a384ca4e2b882697a
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-24 20:40:10 +0300

    solution to task 4, project 0x03-unittests_and_integration_tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,8 +2,8 @@
 """test_client module
 """
 import unittest
-from unittest.mock import patch
 from parameterized import parameterized
+from unittest.mock import patch
 
 from client import GithubOrgClient
 
@@ -12,16 +12,23 @@ class TestGithubOrgClient(unittest.TestCase):
     """TestGithubOrgClient class
     """
 
-    @parameterized.expand(['google', 'abc'])
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+        ])
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
         """test_org function
-        
+
         Args:
             org_name (String): organization name
             mock_get_json ([type]): [description]
         """
-        mock_get_json.return_value = {'payload': True}
+        mock_get_json.return_value = {'login': org_name}
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, mock_get_json.return_value)
         mock_get_json.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
