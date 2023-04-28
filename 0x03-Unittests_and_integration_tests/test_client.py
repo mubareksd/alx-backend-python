e80bcf3b1148ea2200ba24353fe0ba9e19c43d11
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-28 18:07:09 +0300

    task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,29 +1,16 @@
-#!/usr/bin/env python3
-"""test_client module that tests client module
-"""
 import unittest
 from unittest.mock import patch
-from parameterized import parameterized
-
 from client import GithubOrgClient
+from parameterized import parameterized
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """TestGithubOrgClient class that tests GithubOrgClient class
-    """
     @parameterized.expand([
-        ('google'),
-        ('abc')
+        ("google",),
+        ("abc",),
     ])
-    @patch('client.GithubOrgClient.get_json')
+    @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
-        """test_org method that tests org method
-
-        Args:
-            org_name (str): org name
-            ret (Dict): return value
-            mock_get_json (MagicMock): mock get_json
-        """
         mock_get_json.return_value = {'login': org_name}
-        client = GithubOrgClient(org_name)
-        self.assertEqual(client.org, mock_get_json.return_value)
+        org = GithubOrgClient(org_name)
+        assert org.org == mock_get_json.return_value
