c16486a9e235a9e9432457b383425fd9a001ad3d
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-28 19:44:16 +0300

    task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """test_client module that tests client module
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import PropertyMock, patch
 from typing import Dict
 from parameterized import parameterized
 
@@ -30,3 +30,13 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, expected)
         mock_get_json.assert_called_once()
+
+    def test_public_repos_url(self):
+        """test_public_repos_url function that tests _public_repos_url
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            org = {"repos_url": "world"}
+            mock_org.return_value = org
+            client = GithubOrgClient("test")
+            self.assertEqual(client._public_repos_url, org["repos_url"])
