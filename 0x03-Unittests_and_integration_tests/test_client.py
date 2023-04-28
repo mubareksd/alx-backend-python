c542bc7aca26ad35bfb485ddf697c42cfaf13e38
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-28 19:13:30 +0300

    task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
+"""test_client module that tests client module
+"""
 import unittest
 from unittest.mock import patch
+from parameterized import parameterized
 
 
 class TestGithubOrgClient(unittest.TestCase):
+    """TestGithubOrgClient class that tests GithubOrgClient class
+    """
+    @parameterized.expand([
+        "google",
+        "abc"
+    ])
     @patch('client.get_json')
-    def test_org(self):
+    def test_org(self,org_name: str,
+                 mock_get_json: unittest.mock.Mock) -> None:
+        """test_org function that tests org method
+        """
         pass
