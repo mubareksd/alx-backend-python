828b12a48423887044e02d68f1a35e436a6d77e7
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-28 19:19:47 +0300

    task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,6 +5,8 @@ import unittest
 from unittest.mock import patch
 from parameterized import parameterized
 
+from client import GithubOrgClient
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """TestGithubOrgClient class that tests GithubOrgClient class
@@ -14,8 +16,14 @@ class TestGithubOrgClient(unittest.TestCase):
         "abc"
     ])
     @patch('client.get_json')
-    def test_org(self,org_name: str,
+    def test_org(self, org_name: str,
                  mock_get_json: unittest.mock.Mock) -> None:
         """test_org function that tests org method
+
+        Args:
+            org_name (str): org name
+            mock_get_json (unittest.mock.Mock): mock get_json
         """
-        pass
+        mock_get_json.return_value = {"login": org_name}
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, mock_get_json.return_value)
